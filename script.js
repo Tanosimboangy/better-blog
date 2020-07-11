05fed2394ce1f87db8781986bd014c88b9aea42f
Tanosimboangy <jacquit.aja@onja.org>
2020-07-11 10:50:56 +0300

    creating new elements for the new post

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
-console.log('it works');
+const newPost = `
+    <div class="card">
+        <img class="card-img-top" src="https://picsum.photos/500/200"
+        alt="Card image cap">
+        </img>
+        <div class="card-body">
+            <h5 class="card-title">
+            </h5>
+            <p class="card-text">
+            </p>
+            <button type="button" class="btn btn-sm btn-light btn-delete">
+            Delete entry
+            </button>
+        </div>
+        <div  class="card-footer text-muted">
+            10/07/2020
+        </div>
+    </div>
+`;
+
+const postList = document.getElementById('post-list');
+// postList.insertAdjacentHTML('afterbegin', newPost);
+console.log(newPost);
+
+const PostTitle = document.getElementById(postTitle);
+
+
+
+
+
+
+// const postList = document.getElementById('post-list');
+// const card = document.createElement('div');
+// card.classList.add('card');
+// const newImage = document.createElement('img');
+// newImage.classList.add('card-img-top');
+// newImage.alt = "Card image cap";
+// newImage.src = 'https://picsum.photos/500/200';
+// const newCardBody = document.createElement('div');
+// newCardBody.classList.add('card-body');
+// const newCardTitle = document.createElement('h5');
+// newCardTitle.classList.add('card-title');
+// const newCardText = document.createElement('p');
+// newCardText.classList.add('card-text');
+// newCardBody.appendChild(newCardTitle);
+// newCardBody.appendChild(newCardText);
+// card.appendChild(newImage);
+// card.appendChild(newCardBody);
+// postList.appendChild(card);
+
+// console.log(postList);
+ 
